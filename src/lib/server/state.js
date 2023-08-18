43069ab0f26ebf5e7e60401797f5d25a72e8f649
Bill Castillo <bill.castillo@accenture.com>
2023-08-18 20:54:15 +0800

    add state

---
+++ src/lib/server/state.js
@@ -0,0 +1,22 @@
+import { writable, get } from 'svelte/store';
+
+const localState = localStorage.getItem('state');
+const initialState = {
+	quiz: new Map()
+};
+
+if (!localState) {
+	localStorage.setItem('state', JSON.stringify(initialState));
+	// Set localStorage "state" to the "initialState"
+}
+
+const appState = localState ? JSON.parse(localState) : initialState;
+
+export const state = writable(appState);
+
+export const update = (callback) => {
+	const updatedState = callback(get(state));
+
+	state.update(() => updatedState);
+	localStorage.setItem('state', JSON.stringify(updatedState));
+};
